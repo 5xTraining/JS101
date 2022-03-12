464d0952f54f619c6a406207a1b488b3cfd8f4d4
eddie <eddie@5xcampus.com>
2022-03-12 17:44:21 +0800

    完成簡易計數器

--- 07-簡易計數器/app.js
+++ 07-簡易計數器/app.js
@@ -1,1 +1,18 @@
 // 程式碼寫在這裡
+document.addEventListener("DOMContentLoaded", () => {
+  const plusBtn = document.querySelector("#plus")
+  const minusBtn = document.querySelector("#minus")
+  const counter = document.querySelector("#counter")
+
+  plusBtn.addEventListener("click", () => {
+    let currentValue = parseInt(counter.value)
+    counter.value = currentValue + 1
+  })
+
+  minusBtn.addEventListener("click", () => {
+    let currentValue = parseInt(counter.value)
+    if (currentValue > 0) {
+      counter.value = currentValue - 1
+    }
+  })
+})
