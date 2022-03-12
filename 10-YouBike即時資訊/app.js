6e641edb283940972dee02ddef3c230cbc5b053e
Eddie <eddie@5xcampus.com>
2022-03-12 17:44:21 +0800

    使用 jQuery 改寫 YouBike 即時資訊練習

--- 10-YouBike即時資訊/app.js
+++ 10-YouBike即時資訊/app.js
@@ -1,36 +1,34 @@
 // 程式碼寫這裡
 
-document.addEventListener("DOMContentLoaded", () => {
-  const keyword = document.querySelector("#searchKeyword")
-  const form = document.querySelector("#searchForm")
+$().ready(() => {
+  const keyword = $("#searchKeyword")
+  const form = $("#searchForm")
 
-  form.addEventListener("submit", (e) => {
+  form.submit((e) => {
     // 把原本 Form 的行為先停下來
     e.preventDefault()
 
-    const query = keyword.value.trim()
+    const query = keyword.val()
 
     if (query !== "") {
-      const api =
+      const url =
         "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
 
-      fetch(api)
-        .then((resp) => resp.json())
-        .then((sites) => {
-          const bikeList = document.querySelector(".siteList")
-          bikeList.innerHTML = ""
+      $.ajax({ url }).done((sites) => {
+        const bikeList = $(".siteList")
+        bikeList.html = ""
 
-          sites
-            .filter((site) => site.ar.includes(query))
-            .forEach((site) => {
-              const item = `<li class="list-group-item fs-5">
+        sites
+          .filter((site) => site.ar.includes(query))
+          .forEach((site) => {
+            const item = `<li class="list-group-item fs-5">
                 <i class="fas fa-bicycle"></i>
                 ${site.sna.replace("YouBike2.0_", "")} (${site.sbi})<br />
                 <small class="text-muted">${site.ar}</small>
               </li>`
-              bikeList.insertAdjacentHTML("beforeend", item)
-            })
-        })
+            bikeList.append(item)
+          })
+      })
     }
   })
 })
