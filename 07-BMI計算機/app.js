4d8c477b276c1d764e8e1f3631661523e383c85f
eddie <eddie@5xcampus.com>
2022-03-12 17:44:21 +0800

    完成 BMI 計算機

--- 07-BMI計算機/app.js
+++ 07-BMI計算機/app.js
@@ -1,2 +1,22 @@
 // 程式碼寫在這裡
 // 提示：BMI = 體重(kg) / 身高(m) 平方
+
+function bmiCalculator(height, weight) {
+  var w = parseInt(weight)
+  var h = parseInt(height) / 100
+  return (w / (h * h)).toFixed(2)
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.querySelector("button")
+
+  btn.addEventListener("click", () => {
+    const height = document.querySelector("#bodyHeight").value
+    const weight = document.querySelector("#bodyWeight").value
+    const resultArea = document.querySelector("#resultText")
+
+    if (height != "" && weight != "") {
+      resultArea.innerHTML = bmiCalculator(height, weight)
+    }
+  })
+})
